e604c5fd02fb06becf86566271b1262afffc204b
Jonas Enge <jonas.maccyber.enge@nav.no>
2024-03-24 00:47:14 +0100

    6.0.9

---
+++ components/Wrapper.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="wrapper"
+    :style="style"
+  >
+    <v-container>
+      <slot />
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    background: {
+      type: String,
+      default: 'radial-gradient(70% 70% at 50% 100%, rgb(232, 232, 232) 0px, rgb(250, 250, 250) 100%)'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.wrapper {
+  border-top: 1px solid #eaeaea;
+  border-bottom: 1px solid #eaeaea;
+}
+</style>
