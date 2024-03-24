ebf3b1f8a4bcb3ddb0adbd8116320b8149b89dae
Jonas Enge <jonas.maccyber.enge@nav.no>
2024-03-24 20:56:18 +0100

    F**k tooltip

--- components/ShareLinks.vue
+++ components/ShareLinks.vue
@@ -1,35 +1,20 @@
 <template>
   <div class="pt-5">
-    <v-tooltip bottom>
-      <template v-slot:activator="{ props }">
-        <a v-bind="props" @click="copyLink(`https://bigfive-test.com/${id}`)">
-          <v-icon x-large color="black">mdi-link</v-icon>
-        </a>
-      </template>
-      {{ $t('shareLinks.copyLink') }}
-    </v-tooltip>
+    <a @click="copyLink(`https://bigfive-test.com/${id}`)" aria-label="Copy link" icon="mdi-link">
+      <v-icon icon="mdi-link" color="black"></v-icon>
+    </a>
 
-    <v-tooltip bottom>
-      <template v-slot:activator="{ props }">
-        <a :href="'https://www.facebook.com/sharer/sharer.php?u=https://bigfive-test.com/' + id" target="_blank"
-          rel="noopener" aria-label="Share on facebook" v-bind="props">
-          <v-icon x-large color="black">mdi-facebook</v-icon>
-        </a>
-      </template>
-      {{ $t('shareLinks.shareFacebook') }}
-    </v-tooltip>
+    <a :href="'https://www.facebook.com/sharer/sharer.php?u=https://bigfive-test.com/' + id" target="_blank"
+      rel="noopener" aria-label="Share on facebook" icon="mdi-facebook">
+      <v-icon icon="mdi-facebook" color="black"></v-icon>
+    </a>
 
-    <v-tooltip bottom>
-      <template v-slot:activator="{ props }">
-        <a :href="'https://twitter.com/intent/tweet?text=See my personality traits!&url=https://bigfive-test.com/' + id"
-          target="_blank" rel="noopener" aria-label="Share on twitter" v-bind="props">
-          <v-icon x-large color="black">mdi-twitter</v-icon>
-        </a>
-      </template>
-      {{ $t('shareLinks.shareTwitter') }}
-    </v-tooltip>
+    <a :href="'https://twitter.com/intent/tweet?text=See my personality traits!&url=https://bigfive-test.com/' + id"
+      target="_blank" rel="noopener" aria-label="Share on twitter" icon="mdi-twitter">
+      <v-icon icon="mdi-twitter" color="black"></v-icon>
+    </a>
 
-    <v-dialog v-model="dialog" hide-overlay persistent width="155" origin="top left" transition="fade-transition">
+    <v-dialog v-model="dialog" hide-overlay persistent width="155" origin="top left">
       <v-card color="secondary" dark>
         <v-card-title>
           {{ $t('shareLinks.copiedLink') }}
@@ -43,6 +28,14 @@
 import { sleep } from '../lib/helpers'
 const dialog = ref(false)
 
+const props = defineProps({
+  id: {
+    type: String,
+    default: ''
+  }
+})
+const { id } = props
+
 async function copyLink(str) {
   const el = document.createElement('textarea')
   el.value = str
@@ -55,13 +48,6 @@ async function copyLink(str) {
   await sleep(600)
   this.dialog = false
 }
-const props = defineProps({
-  id: {
-    type: String,
-    default: ''
-  }
-})
-const id = props.id
 </script>
 
 <style scoped>
