26c5b3b227a977bf45a0fc2713f7cd111c6e6b73
Jonas Enge <jonas.maccyber.enge@nav.no>
2024-03-24 15:44:20 +0100

    Convert wrapper to vue3

---
+++ components/ShareLinks.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="pt-5">
+    <v-tooltip bottom>
+      <template #activator="{ on }">
+        <a v-on="on" @click="copyLink(`https://bigfive-test.com/${id}`)">
+          <v-icon x-large>{{ mdiLink }}</v-icon>
+        </a>
+      </template>
+      {{ $t('shareLinks.copyLink') }}
+    </v-tooltip>
+
+    <v-tooltip bottom>
+      <template #activator="{ on }">
+        <a :href="'https://www.facebook.com/sharer/sharer.php?u=https://bigfive-test.com/' + id" target="_blank"
+          rel="noopener" aria-label="Share on facebook" v-on="on">
+          <v-icon x-large>{{ mdiFacebook }}</v-icon>
+        </a>
+      </template>
+      {{ $t('shareLinks.shareFacebook') }}
+    </v-tooltip>
+
+    <v-tooltip bottom>
+      <template #activator="{ on }">
+        <a :href="'https://twitter.com/intent/tweet?text=See my personality traits!&url=https://bigfive-test.com/' + id"
+          target="_blank" rel="noopener" aria-label="Share on twitter" v-on="on">
+          <v-icon x-large>{{ mdiTwitter }}</v-icon>
+        </a>
+      </template>
+      {{ $t('shareLinks.shareTwitter') }}
+    </v-tooltip>
+
+    <v-dialog v-model="dialog" hide-overlay persistent width="155" origin="top left" transition="fade-transition">
+      <v-card color="secondary" dark>
+        <v-card-title>
+          {{ $t('shareLinks.copiedLink') }}
+        </v-card-title>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script setup>
+import { mdiFacebook, mdiTwitter, mdiLink } from '@mdi/js'
+import { sleep } from '../lib/helpers'
+const dialog = ref(false)
+
+async function copyLink(str) {
+  const el = document.createElement('textarea')
+  el.value = str
+  el.setAttribute('readonly', '')
+  el.style = { position: 'absolute', left: '-9999px' }
+  document.body.appendChild(el)
+  el.select()
+  document.execCommand('copy')
+  this.dialog = true
+  await sleep(600)
+  this.dialog = false
+}
+const props = defineProps({
+  id: {
+    type: String,
+    default: ''
+  }
+})
+</script>
+
+<style scoped>
+a {
+  margin-right: 30px;
+}
+</style>
--- components/Wrapper.vue
+++ components/Wrapper.vue
@@ -1,23 +1,19 @@
 <template>
-  <div
-    class="wrapper"
-    :style="style"
-  >
+  <div class="wrapper" :style="{ background }">
     <v-container>
       <slot />
     </v-container>
   </div>
 </template>
 
-<script>
-export default {
-  props: {
-    background: {
-      type: String,
-      default: 'radial-gradient(70% 70% at 50% 100%, rgb(232, 232, 232) 0px, rgb(250, 250, 250) 100%)'
-    }
+<script setup>
+const props = defineProps({
+  background: {
+    type: String,
+    default: 'radial-gradient(70% 70% at 50% 100%, rgb(232, 232, 232) 0px, rgb(250, 250, 250) 100%)'
   }
-}
+})
+const background = ref(props.background)
 </script>
 
 <style scoped>
